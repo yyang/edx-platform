e086ccd4de022766a8bb2ec7bf0ffd759f9a3f42
Minh Tue Vo <mvo@edx.org>
2014-06-24 11:52:24 -0400

    some works

--- test.py
+++ test.py
@@ -2,5 +2,11 @@ __author__ = 'minhtuevo'
 
 from django.conf import settings
 import os
-print "the size of settings:", settings.__sizeof__()
-os.system("/edx/app/edxapp/venvs/edxapp/bin/paver test_python")
+print "the total size of settings:", settings.__sizeof__()
+
+print os.environ
+os.environ['PATH'] += ":/edx/app/edxapp/venvs/edxapp/bin"
+print os.environ['PATH']
+# os.system("/edx/app/edxapp/venvs/edxapp/bin/activate")
+os.system
+os.system("paver test_python")
